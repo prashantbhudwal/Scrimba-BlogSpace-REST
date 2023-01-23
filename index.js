6494927695de7b17ec418dfd00a27aab8c6fd5ee
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-01-23 09:15:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+
+
+const log = (prompt="Testing Testing") => console.log(prompt)
+const $ = (id) => document.getElementById(id)
+
+const element = {
+  blogContainer: $('blog-container'),
+  newPostBtn:$('new-post-btn'),
+  modalOverlay:$('modal-overlay'),
+  discardBtn:$('discard-btn'),
+  publishBtn:$('publish-btn'),
+  title:$('title'),
+  content:$('content-area')
+}
+
+const url = {
+  base: "https://apis.scrimba.com/jsonplaceholder",
+  posts: "/posts",
+  todos:"/todos"
+}
+
+
+const renderBlog = (blogArray) => {
+  blogArray.forEach((item) => {
+    const postHtml = `<div class = "post-card">
+                      <h1>${item.title.slice(0, 40)}</h1>
+                      <p>${item.body.slice(0, 300)}</p>
+                      </div>
+                        `
+
+    element.blogContainer.innerHTML += postHtml
+
+  })
+
+}
+
+
+fetch(url.base + url.posts, { method: `GET` })
+  .then(response => response.json())
+  .then(json => {
+    return renderBlog(json.slice(0, 50))
+  }
+  )
+
+
+element.newPostBtn.addEventListener('click',()=>element.modalOverlay.style.display = 'block')
+
+element.discardBtn.addEventListener('click',()=>element.modalOverlay.style.display = 'none')
+
+element.publishBtn.addEventListener('click',(event)=>{
+  event.preventDefault();
+  
+  let draftData ={
+    title:element.title.value,
+    content:element.content.value
+  }
+  publish(draftData);
+})
+
+
+const publish = (draftData) =>{
+  fetch(url.base + url.todos, { 
+    method: `POST`,
+    body:JSON.stringify(draftData)})
+    
+  .then(response => response.json())
+  .then(json => log(json))
+  
+}
